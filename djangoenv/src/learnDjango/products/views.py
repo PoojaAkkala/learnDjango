ccb87168a5211ce74f0cdc4a42fc7b0deb981b31
pooja <poojaakkala2916@gmail.com>
2023-05-04 08:25:25 +0500

    Made my first form

--- djangoenv/src/learnDjango/products/views.py
+++ djangoenv/src/learnDjango/products/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
 from .models import Product
-
+from .forms import ProductCreateForm
 # Create your views here.
 def product_detail_view(request):
     obj = Product.objects.get(id=1)
     context={'object':obj}
     return render(request,"products/detail.html",context)
+
+def product_create_view(request):
+    form=ProductCreateForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+    context={'form':form}
+    return render(request,"products/product_create.html",context)
+
